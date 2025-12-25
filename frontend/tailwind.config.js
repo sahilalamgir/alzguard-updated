220bc6936d1eddd0f7a684c5a9aae7ede5b0e943
Sahil Alamgir <sahil.alamgir@gmail.com>
2025-12-24 19:38:28 -0500

    make alias

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-primary": "#EFECE3",
+        "text-primary": "#000000",
+        "text-secondary": "#4A70A9",
+        accent: {
+          DEFAULT: "#8FABD4",
+          hover: "#7A98C0",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
