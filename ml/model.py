bbe0a113dc88e3d1cea07f8102f0b22a4d6729ec
Sahil Alamgir <sahil.alamgir@gmail.com>
2025-12-26 23:43:55 -0500

    step 3: model

---
+++ ml/model.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from config import NUM_CLASSES
+
+def build_cnn():
+    model = models.Sequential()
+
+    model.add(layers.Conv2D(32, (3,3), activation="relu", input_shape=(224,224,3)))
+    model.add(layers.MaxPooling(2,2))
+
+    model.add(layers.Conv2D(64, (3,3), activation="relu", input_shape=(224,224,3)))
+    model.add(layers.MaxPooling(2,2))
+
+    model.add(layers.Conv2D(128, (3,3), activation="relu", input_shape=(224,224,3)))
+    model.add(layers.MaxPooling(2,2))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation="relu"))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(NUM_CLASSES, activation="softmax"))
+
+    return model
